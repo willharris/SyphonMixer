984a74f75e9e3fcac3934e0f02889b814016cc40
Will Harris <will@harris.ch>
2024-12-14 23:42:26 +0100

    Use toggle menus for floating and full screen commands

--- SyphonMixer/SyphonMixer/SyphonMixerApp.swift
+++ SyphonMixer/SyphonMixer/SyphonMixerApp.swift
@@ -11,6 +11,8 @@ import SwiftUI
 @main
 struct SyphonMixerApp: App {
     @StateObject private var syphonManager = SyphonManager()
+    @State private var isFloating = false
+    @State private var isFullScreen = false
 
     init() {
         // Disable the full screen menu item because it disappears anyway as soon as you
@@ -29,15 +31,31 @@ struct SyphonMixerApp: App {
         .windowResizability(WindowResizability.contentMinSize)
         .commands {
             CommandGroup(after: .toolbar) {
-                Button("Enter Full Screen") {
-                    if let window = NSApplication.shared.windows.first(where: { $0.identifier?.rawValue == "main" }) {
-                        window.toggleFullScreen(nil)
-                        window.makeKeyAndOrderFront(nil)  // Brings window to front and gives it focus
-                        NSApplication.shared.activate(ignoringOtherApps: true)  // Ensures your app is frontmost
+                Toggle("Float Window", isOn: Binding(
+                    get: { isFloating },
+                    set: { newValue in
+                        if let window = NSApplication.shared.windows.first(where: { $0.identifier?.rawValue == "main" }) {
+                            window.level = newValue ? .floating : .normal
+                            isFloating = newValue
+                        }
                     }
-                }
+                ))
+                .keyboardShortcut("f", modifiers: [.command, .option])
+
+                Toggle("Full Screen", isOn: Binding(
+                    get: { isFullScreen },
+                    set: { newValue in
+                        isFullScreen = newValue
+                        if let window = NSApplication.shared.windows.first(where: { $0.identifier?.rawValue == "main" }) {
+                            window.toggleFullScreen(newValue)
+                            window.makeKeyAndOrderFront(nil)  // Brings window to front and gives it focus
+                            NSApplication.shared.activate(ignoringOtherApps: true)  // Make frontmost
+                        }
+                    }
+                ))
                 .keyboardShortcut("f", modifiers: [.command, .control])
             }
+            
         }
 
         Settings {
